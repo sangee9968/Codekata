18ce79a8d3bb8c01c5c7c1cd00501e9d75ee9189
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-01-08 20:32:03 +0530

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,8 +1,8 @@
 n,k=map(int,input().split())
-for num in range(n,k+1):
+for num in range(n+1,k):
    if num>1:
       for j in range(2,num):
          if num%j==0:
             break
       else:
-         print(num)
+         print(num,end=" ")
