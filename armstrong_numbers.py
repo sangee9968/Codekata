f8250ce4bf21ac40b5f47ba605b2cad0284c2eeb
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-01-08 23:13:08 +0530

    Create armstrong_numbers.py

---
+++ armstrong_numbers.py
@@ -0,0 +1,13 @@
+s=""
+n,k=map(int,input().strip().split())
+for i in range(n,k+1):
+   rev=0
+   temp=i
+   while temp>0:
+      r=temp%10
+      rev=rev+(r*r*r)
+      temp=temp//10
+   if i==rev:
+      s=s+str(i)+" "
+w=s.strip()      
+print(w)
