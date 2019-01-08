3e163ef7b3cc35ef56557463590ae2f115a08479
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-01-08 16:02:59 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,9 +1,7 @@
+s=""
 n,k=map(int,input().strip().split())
 for i in range(n+1,k):
-   if i!=k-1:
       if i%2==0:
-         print(i,end=" ")
-   else:
-      if i%2==0:
-         print(i)
-      
+         s=s+str(i)+" "
+w=s.strip()
+print(w)
